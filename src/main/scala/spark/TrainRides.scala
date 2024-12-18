0f6e97912577e75d9e01eb4d9d86af864d9d372f
Obicnwokolo <nwokolo.obinna10@gmail.com>
2024-12-18 20:21:22 +0000

    commit2

--- src/main/scala/spark/TrainRides.scala
+++ src/main/scala/spark/TrainRides.scala
@@ -2,7 +2,27 @@ package spark
 
 import org.apache.spark.{SparkConf, SparkContext}
 
+object TrainRides{
+  def main(args: Array[String]): Unit = {
+    // Step 1: Initialize Spark Session
+    val spark = SparkSession.builder()
+      .appName {
+        "Spark Transformations Example"
+      }
+      .master("local[*]") // Run locally with all available cores
+      .getOrCreate()
 
-object TrainRides {
+    // Step 2: Load Data from CSV
+    val filePath = "C:/Users/chigb/Downloads/railway.csv"
+    val data = spark.read
+      .option("header", "true") // File has headers
+      .option("inferSchema", "true") // Infer data types
+      .csv(filePath)
 
+    println("Original Data:")
+    data.show()
+
+
+    spark.stop()
+  }
 }
